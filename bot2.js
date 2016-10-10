0d599946099a9c5c5040df9a52683aecae45f753
rs285 <rs1646@truman.edu>
2016-10-10 02:24:35 -0500

    first commit

---
+++ bot2.js
@@ -0,0 +1,39 @@
+console.log("This bot is starting now!");
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+var stream = T.stream('user');
+
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+	console.log("Follow event!");
+	var name = eventMsg.source.name;
+	var screenName = eventMsg.source.screen_name;
+	tweetIt('.@' + screenName + ' thank you for following me. Do you like unicorns?');
+}
+
+function tweetIt(txt) {
+
+	var r = Math.floor(Math.random()*100);
+
+	var tweet = {
+		status:txt
+		// place_id: 'ChIJP5iLHkCuEmsRwMwyFmh9AQU'
+	}
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log("Something went wrong!");
+		}
+		else {
+			console.log("It worked!");
+		}
+	}
+}
+
---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         'YasbZKOwQIaOJeeRJQn3fB197',
+  consumer_secret:      'sZMzvQYYEzTrrczSMy5pdhrcHiQ9iAtWEhduO5KIOtSlogj5aX',
+  access_token:         '784653034966003712-pIxMoBr8NoaFTKop0AdArpmGf50WDwX',
+  access_token_secret:  'ZWji17JXu7PgLXGbVpQSxUpGi8OPlyrAW1RiUzp631WjH', 
+}
